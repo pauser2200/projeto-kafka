91189b1ef8eef855ebabeb2f616dbf4ad3919e94
Paulo <pauser2200@gmail.com>
2023-04-26 21:46:52 -0300

    Str producer start

---
+++ str-prducer/src/main/java/com/paulo/strprducer/StrPrducerApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.strprducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrPrducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrPrducerApplication.class, args);
+    }
+
+}
