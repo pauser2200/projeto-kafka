faab2c6cf7ac9fc03b01c3e6f2988a9f1529c2d8
Paulo <pauser2200@gmail.com>
2023-04-28 22:02:10 -0300

    Create Payment topic

---
+++ payment-service/src/main/java/com/paulo/paymentservice/config/KafkaAdminConfig.java
@@ -0,0 +1,40 @@
+package com.paulo.paymentservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    /**Criação das configurações do kafkaAdmin
+     *
+     * @return
+     */
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs =  new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    /** criação do topico
+     * @return
+     */
+    @Bean
+    public KafkaAdmin.NewTopics newTopics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("payment-topc").partitions(1).build()
+        );
+    }
+}
